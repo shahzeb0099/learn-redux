bf7fd26098fd55ba8d43cedae6641fd3886d91b6
Mohammad Shahzeb <mshahzeb1905050100043@gmail.com>
2024-10-31 00:11:15 +0530

    add learning reference

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./slice/counter";
+import redux from "./redux.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={redux} alt="redux" style={{ width: "50%", height: "50%" }} />
         <Counter />
       </header>
     </div>
